ff036bd40782ae3bc22b2f00ccac5a3bad8398ce
Nguyen Van Binh (CME.MSA) <BinhNV12@fsoft.com.vn>
2018-05-17 14:53:31 +0700

    New components for homepage

--- client/src/app/components/Header/Header.jsx
+++ client/src/app/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { withRouter, NavLink } from "react-router-dom";
+import Navigation from "../Nav";
 
 class Header extends React.Component {
   constructor() {
@@ -9,16 +10,17 @@ class Header extends React.Component {
 
   render() {
     return (
-      <div>
-        <ul>
+      <React.Fragment>
+        {/* <ul>
           <li>
             <NavLink exact to="/login">Login</NavLink>
           </li>
           <li>
             <NavLink exact to="/signup">Sign Up</NavLink>
           </li>
-        </ul>
-      </div>
+        </ul> */}
+        <Navigation />
+      </React.Fragment>
     );
   }
 }
